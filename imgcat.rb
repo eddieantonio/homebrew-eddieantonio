b13b94a9a2e52773a56c3b53e8ffb49931ee78ca
Eddie Antonio Santos <easantos@ualberta.ca>
2016-08-01 10:57:42 -0600

    imgcat: use sha256
    Fixes #1.

--- imgcat.rb
+++ imgcat.rb
@@ -1,7 +1,7 @@
 class Imgcat < Formula
   homepage "https://github.com/eddieantonio/imgcat"
   url "https://github.com/eddieantonio/imgcat/archive/v1.1.0.tar.gz"
-  sha1 "584307473d9577df5f8ff1e3d3a89df2bcba4ab2"
+  sha256 "1c171ba432281b9febb698c82c000e4b33918de2c95c4ee38be7db027d85764c"
 
   depends_on "scons" => :build
 
