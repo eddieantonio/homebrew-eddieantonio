bbe23058dc8b9399e0b9322ace41cbe39214fa0b
Eddie Antonio Santos <easantos@ualberta.ca>
2017-01-04 09:41:01 -0700

    isri-ocr-evaluation-tools 6.1.2

--- isri-ocr-evaluation-tools.rb
+++ isri-ocr-evaluation-tools.rb
@@ -1,8 +1,8 @@
 class IsriOcrEvaluationTools < Formula
   desc "The ISRI Analytic Tools for OCR Evalutaion"
   homepage "https://code.google.com/p/isri-ocr-evaluation-tools/"
-  url "https://github.com/eddieantonio/isri-ocr-evaluation-tools/archive/v6.1.1.tar.gz"
-  sha256 "b166bc7bcf56bcfc5f7e2c0ee82ad467d57f4a0487afd93526c37d2d6a0e333f"
+  url "https://github.com/eddieantonio/isri-ocr-evaluation-tools/archive/v6.1.2.tar.gz"
+  sha256 "6da4bde45250f35209ccadbf7b1c223040a19c65fca4bf0ba704d03f71214f8e"
   head "https://github.com/eddieantonio/isri-ocr-evaluation-tools.git"
 
   depends_on "utf8proc"
