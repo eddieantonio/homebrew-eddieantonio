f596f3c3ed647c0b75a5f76391c4e1c72922baa4
Eddie Antonio Santos <easantos@ualberta.ca>
2017-06-28 10:09:02 -0600

    imgcat 2.0.1

--- imgcat.rb
+++ imgcat.rb
@@ -1,14 +1,18 @@
 class Imgcat < Formula
+  desc "It's like cat(1), but for images."
   homepage "https://github.com/eddieantonio/imgcat"
-  url "https://github.com/eddieantonio/imgcat/archive/v2.0.0.tar.gz"
-  sha256 "28c9e8bd87d1448748267c38e9c5c4fb9e2c9a307d80c5775bc8919e820a4746"
+  url "https://github.com/eddieantonio/imgcat/archive/v2.0.1.tar.gz"
+  sha256 "049ec5383301188a2daa872623b6b1bb6097ccd4f57ee4f036e26fc853e90243"
   head "https://github.com/eddieantonio/imgcat.git"
 
   depends_on "libpng" => :recommended
   depends_on "jpeg"
+  depends_on "autoconf" => :build
 
   def install
-    system "make", "install" ,"PREFIX=#{prefix}"
+    system "autoconf"
+    system "./configure"
+    system "make", "install", "PREFIX=#{prefix}"
   end
 
   def caveats
@@ -22,7 +26,7 @@ class Imgcat < Formula
   end
 
   test do
-    output = shell_output("#{bin}/imgcat -d 256 #{test_fixtures('test.jpg')}")
+    output = shell_output("#{bin}/imgcat -d 256 #{test_fixtures("test.jpg")}")
     # Ensure that it produces xterm 256 colour output
     assert output =~ /[\x1b]\[48;5;\d+m/
   end
