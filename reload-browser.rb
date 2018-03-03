5ccf63e936885da93a41b57ddd98188741b8ff9f
Eddie Antonio Santos <easantos@ualberta.ca>
2018-03-03 11:21:24 -0700

    reload-browser: attempt to write a test of some kind.

--- reload-browser.rb
+++ reload-browser.rb
@@ -10,15 +10,12 @@ class ReloadBrowser < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test reload`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    # reload-browser has the side-effect of focusing your browser and actually
+    # pressing refresh "duh", so don't do that in the test. Also, it runs with
+    # the wronge privelleges, and doesn't work anyway. And if run with no
+    # arguments, `reload-browser` always returns an error code. The best thing
+    # we can do is check if it's an executable regular file...
+    system "test", "-f", "#{bin}/reload-browser"
+    system "test", "-x", "#{bin}/reload-browser"
   end
 end
