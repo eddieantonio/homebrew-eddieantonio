8d6bb361e14835f2664dfe3fbac3c11458f57d91
Eddie Antonio Santos <easantos@ualberta.ca>
2018-07-26 07:24:09 -0600

    brainmuk: fix audit: 'Description should start with a capital letter'

--- brainmuk.rb
+++ brainmuk.rb
@@ -1,5 +1,5 @@
 class Brainmuk < Formula
-  desc "brainfuck x86 compiler and interpreter"
+  desc "Brainfuck x86 compiler and interpreter"
   homepage "https://github.com/eddieantonio/brainmuk"
   url "https://github.com/eddieantonio/brainmuk/archive/v0.2.2.tar.gz"
   sha256 "20ec1fd7fe9544175583cd9ea3367b36380f95bfc881bb34c678beb014a51c15"
