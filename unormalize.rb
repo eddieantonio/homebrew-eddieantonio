8060155c5b35bb6da34cc91a82f683458c32333a
Eddie Antonio Santos <easantos@ualberta.ca>
2018-07-26 07:34:02 -0600

    unormalize: fix audit: ':python is deprecated. Usage should be "python".'

--- unormalize.rb
+++ unormalize.rb
@@ -4,7 +4,7 @@ class Unormalize < Formula
   url "https://github.com/eddieantonio/unormalize/archive/v0.1.1.tar.gz"
   sha256 "ba8db90deb671c1b00d13b67b5b9dd16ab1c06255f30df8020bb677a4918e308"
 
-  depends_on :python if MacOS.version <= :snow_leopard
+  depends_on "python" if MacOS.version <= :snow_leopard
 
   def install
     ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
