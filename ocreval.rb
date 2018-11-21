c3bbeec92e4cf023de6ad3e0b9ca8b9534133580
Eddie Antonio Santos <easantos@ualberta.ca>
2018-11-21 12:49:36 -0700

    ocreval 7.0.1

--- ocreval.rb
+++ ocreval.rb
@@ -1,8 +1,8 @@
 class Ocreval < Formula
   desc "Tools for OCR evaluation with UTF-8 support and more"
   homepage "https://github.com/eddieantonio/ocreval"
-  url "https://github.com/eddieantonio/ocreval/archive/v7.0.0.tar.gz"
-  sha256 "56ece76a6768c595a0070958f4da95c209796298fecfa4edd746f7844b55be5d"
+  url "https://github.com/eddieantonio/ocreval/archive/v7.0.1.tar.gz"
+  sha256 "00cd54f806d122b1accd32470eb54ae939483708f1ef49838f848714ee18c47a"
   head "https://github.com/eddieantonio/ocreval.git"
 
   depends_on "utf8proc"
