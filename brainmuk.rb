9cba4c67597da99126d2331ec69ba99cfa2f662c
Eddie Antonio Santos <easantos@ualberta.ca>
2019-09-16 17:50:23 -0600

    brainmuk 0.3.0

--- brainmuk.rb
+++ brainmuk.rb
@@ -1,8 +1,8 @@
 class Brainmuk < Formula
   desc "Brainfuck x86 compiler and interpreter"
   homepage "https://github.com/eddieantonio/brainmuk"
-  url "https://github.com/eddieantonio/brainmuk/archive/v0.2.2.tar.gz"
-  sha256 "20ec1fd7fe9544175583cd9ea3367b36380f95bfc881bb34c678beb014a51c15"
+  url "https://github.com/eddieantonio/brainmuk/archive/v0.3.0.tar.gz"
+  sha256 "4191caa7a7d310160e7bb99b2163f57fcc5e25c151b38ff48a4ab04d1e6edac7"
   head "https://github.com/eddieantonio/brainmuk.git"
 
   def install
