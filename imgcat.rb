3c087fb3526ab4a71cbc213db8c03c37d2348519
Eddie Antonio Santos <easantos@ualberta.ca>
2020-11-01 00:20:06 -0600

    imgcat 2.5.0

--- imgcat.rb
+++ imgcat.rb
@@ -1,16 +1,14 @@
 class Imgcat < Formula
   desc "It's like cat(1), but for images."
   homepage "https://github.com/eddieantonio/imgcat"
-  url "https://github.com/eddieantonio/imgcat/archive/v2.3.2.tar.gz"
-  sha256 "995b0039c36fc387a17037334309e36fd67d9869cbebbdd0fc155202d2bceefe"
+  url "https://github.com/eddieantonio/imgcat/releases/download/v2.5.0/imgcat-2.5.0.tar.gz"
+  sha256 "8f18e10464ed1426b29a5b11aee766a43db92be17ba0a17fd127dd9cf9fb544b"
   head "https://github.com/eddieantonio/imgcat.git"
 
   depends_on "libpng" => :recommended
   depends_on "jpeg"
-  depends_on "autoconf" => :build
 
   def install
-    system "autoconf"
     system "./configure"
     system "make", "install", "PREFIX=#{prefix}"
   end
